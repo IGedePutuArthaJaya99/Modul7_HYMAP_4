2cd22da3302bf97392a9ee22c12dccb1af54937c
IGedePutuArthaJaya99 <arthawinangun05@gmail.com>
2026-04-12 21:19:20 +0700

    menambahkan class TeamMembers103082400036

--- Modul7_103082400036/obj/Debug/net10.0/Modul7_103082400036.AssemblyInfo.cs
+++ Modul7_103082400036/obj/Debug/net10.0/Modul7_103082400036.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Modul7_103082400036")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9448e782de1c1556bcb0408983559024fd6838c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Modul7_103082400036")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Modul7_103082400036")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
