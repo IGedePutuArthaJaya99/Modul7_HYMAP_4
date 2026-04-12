c5491dc4afa0177c5083a6acd2200e1e87447a90
IGedePutuArthaJaya99 <arthawinangun05@gmail.com>
2026-04-12 21:01:11 +0700

    First commit

---
+++ Modul7_103082400036/DataMahasiswa103082400036.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace Modul7_103082400036
+{
+    public class Nama
+    {
+        public string depan { get; set; }
+        public string belakang { get; set; }
+    }
+
+    public class Mahasiswa
+    {
+        public Nama nama { get; set; }
+        public long nim { get; set; }
+        public string fakultas { get; set; }
+    }
+
+    public class DataMahasiswa103082400036
+    {
+        public void ReadJSON()
+        {
+          
+            string jsonString = File.ReadAllText("jurnal7_1_103082400036.json");
+            Mahasiswa mhs = JsonSerializer.Deserialize<Mahasiswa>(jsonString);
+
+            Console.WriteLine($"Nama: {mhs.nama.depan} {mhs.nama.belakang}");
+            Console.WriteLine($"NIM: {mhs.nim}");
+            Console.WriteLine($"Fakultas: {mhs.fakultas}");
+        }
+    }
+}
