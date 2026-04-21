0c4ddc05baa50383a2edce31bb51b11dac6e362a
salmn-tkyk <salman.hafidzuddin05@gmail.com>
2026-04-21 23:35:53 +0700

    menambahkan class DataMahasiswa103082400020

---
+++ JurnalModul7/DataMahasiswa103082400020.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace Modul7_103082400020
+{
+    public class Nama
+    {
+        public string depan { get; set; }
+        public string belakang { get; set; }
+    }
+
+
+    public class Alamat
+    {
+        public string streetAddress { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+    }
+
+    public class MataKuliah
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Mahasiswa
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public Alamat address { get; set; }
+        public List<MataKuliah> courses { get; set; }
+    }
+
+    public class DataMahasiswa103082400020
+    {
+        public void ReadJSON()
+        {
+            string jsonString = File.ReadAllText("jurnal7_1_103082400020.json");
+            Mahasiswa mhs = JsonSerializer.Deserialize<Mahasiswa>(jsonString);
+
+            Console.WriteLine($"Nama: {mhs.firstName} {mhs.lastName}");
+            Console.WriteLine($"Gender: {mhs.gender}");
+            Console.WriteLine($"Umur: {mhs.age}");
+            Console.WriteLine($"Alamat: {mhs.address.streetAddress}, {mhs.address.city}, {mhs.address.state}");
+            Console.WriteLine("Mata Kuliah:");
+            foreach (var mk in mhs.courses)
+            {
+                Console.WriteLine($"- {mk.code}: {mk.name}");
+            }
+        }
+    }
+}
---
+++ JurnalModul7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Modul7_103082400020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataMahasiswa103082400020 dataMhs = new DataMahasiswa103082400020();
+            dataMhs.ReadJSON();
+
+            Console.WriteLine();
+        }
+    }
+}
